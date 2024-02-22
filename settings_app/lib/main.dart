681e4d1295e327094b9d20151876e653689e3640
Maiki2002 <maiki200202@gmail.com>
2024-02-22 01:27:37 -0600

    add new icons

--- settings_app/lib/main.dart
+++ settings_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'secondscreen.dart';
 import 'InterfaceCamera.dart';
 
@@ -75,10 +76,12 @@ class CardInterface1 extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const ListTile(
-              leading: Icon(
-                Icons.person,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Siri.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Miguel Ángel"),
               subtitle: Text("Apple ID, iCloud+, compras y más"),
@@ -129,34 +132,42 @@ class CardInterface2 extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const ListTile(
-              leading: Icon(
-                Icons.airplanemode_active,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Airplane Mode.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Modo vuelo"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.wifi,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Wi-Fi.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Wi-Fi"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.bluetooth,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Bluetooth.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Bluetooth"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.signal_cellular_alt,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Cellular.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Red celular"),
               trailing: Icon(Icons.navigate_next, size: 30),
@@ -194,34 +205,42 @@ class CardInterface3 extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const ListTile(
-              leading: Icon(
-                Icons.notifications,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Notifications.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Notificaciones"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.vibration,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Sound.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Sonidos y vibración"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.nightlight_round,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Focus Mode.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Enfoque"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.hourglass_empty,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Screen Time.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Tiempo en pantalla"),
               trailing: Icon(Icons.navigate_next, size: 30),
@@ -259,39 +278,47 @@ class CardInterface4 extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const ListTile(
-              leading: Icon(
-                Icons.settings,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/General.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("General"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.toggle_on,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Control Center.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Centro de control"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.brightness_4,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Screen and brigth.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Pantalla y brillo"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.apps,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Init Screen.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Pantalla de inicio y biblioteca de apps"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
+            ListTile(
               leading: Icon(
                 Icons.accessibility,
                 size: 30,
@@ -299,34 +326,42 @@ class CardInterface4 extends StatelessWidget {
               title: Text("Accesibilidad"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.stream,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Wallpaper.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Fondo de pantalla"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.search,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Siri.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Siri y Buscar"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
-            const ListTile(
-              leading: Icon(
-                Icons.fullscreen,
-                size: 30,
+            ListTile(
+              leading: Image.asset(
+                "assets/icons/Face ID.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Face ID y código"),
               trailing: Icon(Icons.navigate_next, size: 30),
             ),
             ListTile(
-              leading: Icon(
-                Icons.hearing,
-                size: 30,
+              leading: Image.asset(
+                "assets/icons/Ear ID.png",
+                width: 30,
+                height: 30,
+                fit: BoxFit.cover,
               ),
               title: Text("Ear ID y código"),
               trailing: Icon(Icons.navigate_next, size: 30),
